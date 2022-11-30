3b8af99aadd052fe1f7e62d4a5df0183c8fcf41f
Adithya Bhat <bhat24@purdue.edu>
2022-11-30 02:11:17 -0500

    Formatting

--- consensus/src/server/leto/chain_state.rs
+++ consensus/src/server/leto/chain_state.rs
@@ -1,10 +1,10 @@
-use std::fmt;
 use super::Leto;
 use crate::types::Block;
 use anyhow::Result;
 use crypto::hash::Hash;
 use log::*;
 use serde::{de::DeserializeOwned, Serialize};
+use std::fmt;
 use storage::rocksdb::Storage;
 
 pub struct ChainState<Tx> {
@@ -34,28 +34,28 @@ where
             store,
         }
     }
-
 }
 
 impl<Tx> ChainState<Tx>
-where 
+where
     Tx: Serialize + fmt::Debug,
 {
     pub async fn genesis_setup(&mut self) -> Result<()> {
         let serialized_genesis = bincode::serialize(&Leto::<Tx>::GENESIS_BLOCK)?;
-        trace!("Writing genesis block: {:?} with hash: {:?} and serialized form: {:?}", 
+        trace!(
+            "Writing genesis block: {:?} with hash: {:?} and serialized form: {:?}",
             Leto::<Tx>::GENESIS_BLOCK,
             self.highest_block_hash,
             serialized_genesis,
         );
         self.store
-            .write(
-                self.highest_block_hash.to_vec(),
-                serialized_genesis.clone(),
-            )
+            .write(self.highest_block_hash.to_vec(), serialized_genesis.clone())
             .await;
         // let (sender, receiver) = oneshot::channel();
-        let res = self.store.notify_read(self.highest_block_hash.to_vec()).await?;
+        let res = self
+            .store
+            .notify_read(self.highest_block_hash.to_vec())
+            .await?;
         assert_eq!(res, serialized_genesis);
         Ok(())
     }
@@ -69,14 +69,11 @@ where
         &mut self,
         parent_hash: Hash<Block<Tx>>,
     ) -> Result<Option<Block<Tx>>> {
-        let result = self.store
-            .read(parent_hash.to_vec())
-            .await?;
+        let result = self.store.read(parent_hash.to_vec()).await?;
         trace!("Got {:?} from reading parent hash", result);
         if let Some(raw) = result {
             return Ok(Some(
-                bincode::deserialize(&raw)
-                    .map_err(anyhow::Error::new)?
+                bincode::deserialize(&raw).map_err(anyhow::Error::new)?,
             ));
         }
         Ok(None)
